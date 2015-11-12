11b320d899f4c42dcac8fb2e957197ed764ca1a6
JesúsAlejandro <JessAlejandro@ALEJANDRO>
2015-11-12 12:21:16 -0500

    archivo5

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -16,7 +16,6 @@ public class JavaApplication5 {
      */
     public static void main(String[] args) {
         System.out.println("modificado :)");
-        System.out.println(":)");
     }
 }
     
